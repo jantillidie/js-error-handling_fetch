fc78acfbc4b04951c8cf658c285994e5670028de
Jan Maaß <git@antipost.de>
2023-11-08 11:05:50 +0100

    Add solution

--- js/index.js
+++ js/index.js
@@ -3,15 +3,33 @@ console.clear();
 const userElement = document.querySelector(".user");
 
 async function loadUser(url) {
+  // Select the error element:
+  const errorElement = document.querySelector(".error")
+  // Clear any existing error messages:
+  errorElement.textContent = '';
   console.log(url);
-  const response = await fetch(url);
-  const json = await response.json();
-  const user = json.data;
 
-  userElement.innerHTML = `
+  // Start the try...catch block to catch errors during the fetch call:
+  try {
+    const response = await fetch(url);
+    // if the response is not okay throw an error with the status text:
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const json = await response.json();
+    const user = json.data;
+
+    userElement.innerHTML = `
   <h2>${user.first_name} ${user.last_name}</h2>
   <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
   `;
+  }
+  // display the error message:
+  catch (error) {
+    errorElement.textContent = error.message;
+  }
+  // if there is no error, the error element stays empty (line 9)
 }
 
 document
